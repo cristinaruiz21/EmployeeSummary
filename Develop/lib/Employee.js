ecb4828185e70a50ead831f7cec81c4af47c0ece
Cristina Ruiz <cristinaruiz96@yahoo.com>
2020-10-19 22:00:32 -0500

    Install pkg 2

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(employee, name, id, email ) {
+        if(!employee){
+            throw new Error ("You are not an employee.");
+        }
+        if(!name){
+            throw new Error ("You are missing the name.");
+        }
+        if (!id){
+
+            throw new Error ("You are missing the id");
+        }
+    }
+}
+
+module.exports = Employee;
